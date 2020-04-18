912a0bdabeeb8625e8c2799653aa35a2fc125c9a
Farrukruz <62787678+Farrukruz@users.noreply.github.com>
2020-04-18 07:51:48 +0300

    indes.js

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,7 +1,6 @@
 function sum(m, n) {
-  // ваш код...
-  m = prompt('Enter some number for m ?')
-  n = prompt('Enter some number for n ?')
   return m * n;
 }
-alert(sum())
+alert(sum(2,5))
+alert(sum(1,6))
+
